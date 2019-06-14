50a222ecb00c58d66c8c806c3e0d5a4d3fb29d7b
Cong Ding <tuxding@gmail.com>
2019-06-14 15:09:47 -0400

    data: init

---
+++ cmd/data.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/scalog/scalogger/data"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// dataCmd represents the data command
+var dataCmd = &cobra.Command{
+	Use:   "data",
+	Short: "The data dissemination layer",
+	Long:  `The data dissemination layer`,
+	Run: func(cmd *cobra.Command, args []string) {
+		data.Start()
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(dataCmd)
+	dataCmd.PersistentFlags().IntP("sid", "s", 0, "Shard index")
+	dataCmd.PersistentFlags().IntP("rid", "r", 0, "Replica index in the shard")
+	viper.BindPFlag("sid", dataCmd.PersistentFlags().Lookup("sid"))
+	viper.BindPFlag("rid", dataCmd.PersistentFlags().Lookup("rid"))
+}
---
+++ data/main.go
@@ -0,0 +1,14 @@
+package data
+
+import (
+	log "github.com/scalog/scalogger/logger"
+
+	"github.com/spf13/viper"
+)
+
+func Start() {
+	numReplica := viper.GetInt("data-replication-factor")
+	batchingInterval := viper.GetInt("data-batching-interval")
+	log.Printf("%v: %v", "data-replication-factor", numReplica)
+	log.Printf("%v: %v", "data-batching-interval", batchingInterval)
+}
