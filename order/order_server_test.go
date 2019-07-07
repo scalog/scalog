43d647cf9c3950a04c536572222fb2ebba63cbf0
Cong Ding <tuxding@gmail.com>
2019-07-07 16:23:09 -0400

    order: fix test

--- order/order_server_test.go
+++ order/order_server_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestNewOrderServer(t *testing.T) {
-	s := NewOrderServer(0, 1, 2, time.Second)
+	s := NewOrderServer(0, 1, 2, time.Second, make([]string, 0))
 	if s == nil {
 		t.Fatal("New OrderServer is nil")
 	}
